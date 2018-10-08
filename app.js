7e92a1993d8811753990888698fa21e9b5037873
sagar gowda <sagardb50@gmaill.com>
2018-10-09 01:53:33 +0530

    added

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ let video;
 let name='';
 
 function setup(){
-    createCanvas(400,300);
+
     video = createCapture(VIDEO);
+    createCanvas(500,50);
     background(1);
     mobilenet = ml5.imageClassifier('MobileNet',video,modelReady);
 }
@@ -29,6 +30,6 @@ function draw(){
     image(video,0,0);
     fill(255);
     background(1);
-    textSize(14);
-    text(name,10,height -20);
+    textSize(24);
+    text(name,10,height -50);
 }
